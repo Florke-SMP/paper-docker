7b1bfe720d4fbc1f26c5f8dba02929e9b31adda0
Florke64 (Daniel) <daniel@florke.pl>
2025-10-10 00:59:17 +0200

    Initial commit

---
+++ dockerfile
@@ -0,0 +1,58 @@
+FROM ubuntu:24.04
+
+LABEL org.opencontainers.image.source="https://www.github.com/Florke-SMP/paper-docker"
+LABEL org.opencontainers.image.description="Docker image for Paper, a Minecraft server software."
+LABEL org.opencontainers.image.licenses="GPL-3.0"
+
+LABEL maintainer="Florke64"
+
+WORKDIR /paper
+VOLUME /paper
+
+RUN mkdir /plugins
+VOLUME /plugins
+
+### Install APT packages ###
+# Adds AWS repo's GPG keys and installs Amazon's Corretto 21.
+# This follows instructions from https://docs.papermc.io/.
+
+ARG JDK_APT_SOURCE="deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main"
+
+RUN apt-get update && apt-get upgrade -y \
+&& apt-get install --no-install-recommends -y \
+	ca-certificates \
+	apt-transport-https \
+	gnupg \
+	wget \
+\
+&& wget -O - https://apt.corretto.aws/corretto.key \
+	| gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg \
+\	
+&& echo "${JDK_APT_SOURCE}" | tee /etc/apt/sources.list.d/corretto.list \
+\
+&& apt-get update && apt-get install \
+	--no-install-recommends -y \
+	java-21-amazon-corretto-jdk \
+	libxi6 \
+	libxtst6 \
+	libxrender1 \
+&& apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Get the Velocity JAR file
+# PAPER_URL is provided by .buildargs
+ARG PAPER_URL
+ARG SHA256
+
+# Download and verify
+RUN wget -O /paper.jar "${PAPER_URL}" && \
+    echo "${SHA256} /paper.jar" | sha256sum -c -
+
+# Entrypoint script
+COPY ./entrypoint.sh /entrypoint.sh
+ENTRYPOINT [ "/entrypoint.sh" ]
+RUN chmod +x /entrypoint.sh
+
+COPY recommended-jvm-flags.txt /paper-jvm-flags.txt
+
+# Expose default proxy's port
+EXPOSE 25565
---
+++ entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+# Supported environment variables:
+# PAPER_RECOMMENDED_JVM_FLAGS: If set to false, skips fetching recommended JVM flags
+# Default: true
+
+# PAPER_JVM_FLAGS: Additional JVM flags to add to the recommended flags
+# Default: (empty)
+
+# PAPER_EULA: Must be set to "true" to accept the Minecraft EULA
+# Default: false
+
+# Default JVM options variable
+RECOMMENDED_JVM_FLAGS=""
+# File to read JVM flags from
+JVM_FLAGS_FILE="/paper-jvm-flags.txt"
+
+function print_welcome() {
+    echo "Starting dockerized Paper (unofficial image)"
+    echo "by Florke64 | https://github.com/Florke64/paper-docker"
+    echo "----------------------------------------------"
+    echo "Running in directory: $(pwd)"
+    echo "Current user: $(whoami)"
+    echo "Current date and time: $(date)"
+}
+
+function read_recommended_jvm_flags() {
+    if [ -s "${JVM_FLAGS_FILE}" ]; then
+        echo "Found ${JVM_FLAGS_FILE} file, loading flags..."
+        RECOMMENDED_JVM_FLAGS=$(cat "${JVM_FLAGS_FILE}")
+    else
+        echo "No ${JVM_FLAGS_FILE} file found, proceeding without specific JVM flags."
+        RECOMMENDED_JVM_FLAGS=""
+    fi
+}
+
+print_welcome
+
+if [ "$PAPER_RECOMMENDED_JVM_FLAGS" = false ]; then
+    echo "The variable PAPER_RECOMMENDED_JVM_FLAGS is false."
+    echo "Skipping loading recommended JVM flags."
+    echo .
+    echo "You can set the PAPER_JVM_FLAGS to use your own JVM flags."
+else
+    echo "Attempting to load recommended JVM flags."
+    echo "Set the environment variable PAPER_RECOMMENDED_JVM_FLAGS to false to skip this step."
+
+    read_recommended_jvm_flags
+fi
+
+PAPER_EULA=${PAPER_EULA:-false}
+echo "eula=${PAPER_EULA}" > eula.txt
+
+read -a RECOMMENDED_JVM_FLAGS < "${JVM_FLAGS_FILE}"
+java "${RECOMMENDED_JVM_FLAGS[@]}" ${PAPER_JVM_FLAGS} -jar /paper.jar nogui --plugins /plugins
